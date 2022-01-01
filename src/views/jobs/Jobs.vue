748613a55f7110f7f984f7ed29ac78cbb055e46f
Ahmed Hossam <a7mad.hossam2299@gmail.com>
2022-01-01 17:07:16 +0200

    first commit

---
+++ src/views/jobs/Jobs.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>jobs</h1>
+  <div v-for="job in jobs" :key="job.id">
+    <h2>{{job.title}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      jobs:[
+        {title:'ninja ux',id:1,details:'lorem'},
+        {title:'ninja web',id:2,details:'lorem'},
+        {title:'ninja vue',id:3,details:'lorem'},
+
+      ]
+    }
+  }
+}
+</script>
